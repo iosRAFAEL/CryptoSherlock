afe9f40a79094a02b6cace2590a8206603181f9d
iosRAFAEL <r.latypov1995@yandex.ru>
2023-06-29 00:22:39 +0300

    Initial Commit

---
+++ CryptoSherlock/CryptoSherlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSherlockApp.swift
+//  CryptoSherlock
+//
+//  Created by BLASTA RECORDZ on 29.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSherlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
