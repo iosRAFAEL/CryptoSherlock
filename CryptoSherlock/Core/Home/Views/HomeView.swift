d70bc67ec3495d2fe9708c391d21fb4228ad8ed5
iosRAFAEL <r.latypov1995@yandex.ru>
2023-07-03 04:38:56 +0300

    Creating HomeView and a navigation header

---
+++ CryptoSherlock/Core/Home/Views/HomeView.swift
@@ -0,0 +1,66 @@
+//
+//  HomeView.swift
+//  CryptoSherlock
+//
+//  Created by RAFAEL on 03.07.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            // background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // content layer
+            VStack {
+                homeHeader
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+       
+    }
+}
+
+extension HomeView {
+    
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+//                        .animation(.none)
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+//                        .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180: 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+    
+}
